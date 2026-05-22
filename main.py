7eead74ec04af2d6a56f570f24122b2d6006e7a8
kavitasatishmahor-coder <kavitasatishmahor@gmail.com>
2026-05-22 14:50:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import time
-import platform
 
 API_ID = 27959583
 API_HASH = "034e953d9e1cfe6b08cc2b2a76c2011e"
 BOT_TOKEN = "8825900558:AAFwkpG7VrrzGAn5gN0ujM8fjofFqjK_5p8"
 
-OWNER_USERNAME = "@brutal_luffy"
-UPDATES_CHANNEL = "https://t.me/brutal_luffy"
-SUPPORT_GROUP = "https://t.me/brutal_luffybot"
-
-START_TIME = time.time()
-
 app = Client(
-    "LuffyyMusicBot",
+    "MegaLuffyBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
@@ -23,55 +14,17 @@ app = Client(
 @app.on_message(filters.command("start"))
 async def start(client, message):
 
-    uptime = int(time.time() - START_TIME)
-
-    text = f"""
-✨ Hello {message.from_user.first_name} 💖
-
-🎵 Welcome To Luffyy Musical Bot 🎵
-
-━━━━━━━━━━━━━━━━━━
-🔥 Premium Music Bot
-⚡ Fastest Downloader
-🎬 HD Video Support
-🎧 Unlimited Songs
-━━━━━━━━━━━━━━━━━━
-
-👨‍💻 Developer : {OWNER_USERNAME}
-⚙️ Status : Online ✅
-⏰ Uptime : {uptime} sec
-
-━━━━━━━━━━━━━━━━━━
-
-🎵 /song = Download Song
-🎬 /video = Download Video
-🏓 /ping = Check Speed
-📚 /help = Help Menu
-
-💖 Enjoy Your Music Journey
-"""
-
-    buttons = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "🎵 Music",
-                    callback_data="music"
-                ),
-                InlineKeyboardButton(
-                    "🎬 Video",
-                    callback_data="video"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "📢 Updates",
-                    url=UPDATES_CHANNEL
-                ),
-                InlineKeyboardButton(
-                    "💬 Support",
-                    url=SUPPORT_GROUP
-                )
-            ]
-        ]
+    await message.reply_text(
+        f"✨ Hello {message.from_user.first_name}\n\n✅ Mega Luffy Musical Bot Is Online!"
     )
+
+@app.on_message(filters.command("ping"))
+async def ping(client, message):
+
+    await message.reply_text(
+        "🏓 Pong!"
+    )
+
+print("✅ Bot Started Successfully")
+
+app.run()
